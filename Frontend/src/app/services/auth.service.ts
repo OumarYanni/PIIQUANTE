6232eb9928962c52fa19d67a4a164809ede459f4
Oumar Yanni <oumaryanni@gmail.com>
2023-11-17 22:49:10 +0000

    Revert "Connectiong the heroku backend to the gh-pages frontend"
    This reverts commit 7f26533cce994940872a8814201820e45e5f99b1.

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -4,20 +4,19 @@ import { BehaviorSubject, tap } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
+
   isAuth$ = new BehaviorSubject<boolean>(false);
   private authToken = '';
   private userId = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient,
+              private router: Router) {}
 
   createUser(email: string, password: string) {
-    return this.http.post<{ message: string }>(
-      'https://salty-ravine-94797-78ee974498c5.herokuapp.com/api/auth/signup',
-      { email: email, password: password }
-    );
+    return this.http.post<{ message: string }>('http://localhost:3000/api/auth/signup', {email: email, password: password});
   }
 
   getToken() {
@@ -29,18 +28,13 @@ export class AuthService {
   }
 
   loginUser(email: string, password: string) {
-    return this.http
-      .post<{ userId: string; token: string }>(
-        'https://salty-ravine-94797-78ee974498c5.herokuapp.com/api/auth/login',
-        { email: email, password: password }
-      )
-      .pipe(
-        tap(({ userId, token }) => {
-          this.userId = userId;
-          this.authToken = token;
-          this.isAuth$.next(true);
-        })
-      );
+    return this.http.post<{ userId: string, token: string }>('http://localhost:3000/api/auth/login', {email: email, password: password}).pipe(
+      tap(({ userId, token }) => {
+        this.userId = userId;
+        this.authToken = token;
+        this.isAuth$.next(true);
+      })
+    );
   }
 
   logout() {
@@ -49,4 +43,5 @@ export class AuthService {
     this.isAuth$.next(false);
     this.router.navigate(['login']);
   }
+
 }
